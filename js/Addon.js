e5de05fcd83ccc39b87d615795df9fe802f211a3
Ethan <48872919+egrayum@users.noreply.github.com>
2020-05-18 18:41:22 -0600

    Update Addon.js

--- js/Addon.js
+++ js/Addon.js
@@ -5,14 +5,16 @@ var bY = 100;
 var solidity = 0;
 var phase = "none";
 
-if (bossFight == true) {
- if (phase = "none") {
-  phase = "spawn"; 
+function boss() {
+ if (bossFight == true) {
+  if (phase = "none") {
+   phase = "spawn"; 
+  }
+  room = 100;
+  if (phase == "spawn") {
+   solidity += 0.02;
+  }
+  ctx.fillStyle = "rgba(255, 30, 30, " + solidity + ")";
+  ctx.fillRect(bX, bY, 100, 75);
  }
- room = 100;
- if (phase == "spawn") {
-  solidity += 0.02;
- }
- ctx.fillStyle = "rgba(255, 30, 30, " + solidity + ")";
- ctx.fillRect(bX, bY, 100, 75);
 }
