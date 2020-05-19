b92a5797867ff1a34948856eae1d6c58a2de4da7
Ethan <48872919+egrayum@users.noreply.github.com>
2020-05-19 13:57:14 -0600

    Update Addon.js

--- docs/Addon.js
+++ docs/Addon.js
@@ -9,6 +9,10 @@ var fY;
 var rounds = 0;
 var fireball = false;
 var fDirect;
+var safeX;
+var safeY;
+var bossHealth = 250;
+var barricadeHealth = 75;
 
 function boss() {
  if (bossFight == true) {
@@ -20,9 +24,11 @@ function boss() {
   if (phase == "spawn" && solidity < 1.1) {
    solidity += 0.01;
   }
+  // triggers phase 1
   if (solidity > 1) {
    phase = "circle"
   }
+  // phase 1- circling
   if (phase == "circle") {
    if (bY < 100 && bX < 625) {
     bX += 8;
@@ -37,6 +43,7 @@ function boss() {
     bY -= 8;
     rounds++;
    }
+   // for firing fireballs
    if (bX > 575 && bY > 225 && bY < 233) {
     fY = bY + 15;
     fX = bX;
@@ -44,10 +51,13 @@ function boss() {
     fDirect = "left";
    }
    if (bX < 75 && bY > 225 && bY < 233) {
+    fY = bY + 15;
+    fX = bX + 55;
     fireball = true;
     fDirect = "right";
    }
   }
+  // fireball
   if (fireball == true) {
    ctx.fillStyle = "#dd1111";
    ctx.fillRect(fX, fY, 45, 45);
@@ -64,6 +74,19 @@ function boss() {
     }
    }
   }
+  if (rounds == 4) {
+   phase = "barricade";
+   rounds = 0;
+  }
+  // barricade phase
+  if (phase == "barricade") {
+   safeX = pX;
+   safeY = pY;
+   ctx.strokeStyle = "#000";
+   ctx.lineWidth = 10;
+   ctx.strokeRect(pX - 25, pY - 25, 100, 100);
+  }
+  // boss
   ctx.fillStyle = "rgba(255, 30, 30, " + solidity + ")";
   ctx.fillRect(bX, bY, 100, 75);
  }
