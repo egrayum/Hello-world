4ae763d42b88d30f2d4dee52ded8db03e10042f0
Ethan <48872919+egrayum@users.noreply.github.com>
2020-05-20 14:44:30 -0600

    Update Addon.js

--- docs/Addon.js
+++ docs/Addon.js
@@ -10,6 +10,8 @@ var rounds = 0;
 var fireball = false;
 var fDirect;
 var bossHealth = 250;
+var dX;
+var dY;
 
 function boss() {
  if (bossFight == true) {
@@ -21,89 +23,36 @@ function boss() {
   if (phase == "spawn" && solidity < 1.1) {
    solidity += 0.01;
   }
-  // triggers phase 1
-  if (solidity > 0.9 && solidity < 1) {
-   phase = "circle"
+  if (solidity > 1 && solidity < 1.05) {
+   phase = "circle";
   }
-  // phase 1- circling
   if (phase == "circle") {
-   if (bY < 100 && bX < 625) {
-    bX += 8;
-   } 
-   if (bY < 450 && bX > 620) {
-    bY += 8;
-   } 
-   if (bY > 425 && bX > 75) {
-    bX -= 8;
-   }
-   if (bY > 70 && bX < 75) {
-    bY -= 8;
-   }
-   if (bX > 250 && bX < 260 && bY < 100) {
-    phase = "barricade";
-   }
-   // for firing fireballs
-   if (bX > 575 && bY > 225 && bY < 233) {
-    fY = bY + 15;
-    fX = bX;
-    fireball = true;
-    fDirect = "left";
-   }
-   if (bX < 75 && bY > 225 && bY < 233) {
-    fY = bY + 15;
-    fX = bX + 55;
-    fireball = true;
-    fDirect = "right";
-   }
+   
   }
-  // fireball
-  if (fireball == true) {
-   ctx.fillStyle = "#dd1111";
-   ctx.fillRect(fX, fY, 45, 45);
-   if (fDirect == "left") {
-    fX -= 7;
-    if (fX < pX + 40 && fY > pY - 45 && fY < pY + 45 || fX < 50) {
-     fireball = false;
-    }
-   }
-   if (fDirect == "right") {
-    fX += 7;
-    if (fX > pX - 40 && fY > pY - 45 && fY < pY + 45 || fX > 705) {
-     fireball = false;
-    }
-   }
-   if (fDirect == "down") {
-    fY += 7;
-    if (fX > pX - 40 && fX < pX + 50 && fY > pY - 40 || fY > 510) {
-     fireball = false;
-    }
-   }
-  }
-  // barricade phase
   if (phase == "barricade") {
-   ctx.strokeStyle = "#000";
-   ctx.lineWidth = 10;
-   ctx.strokeRect(pX - 25, pY - 25, 100, 100);
-   barricaded();
+   
   }
-  // boss
-  ctx.fillStyle = "rgba(255, 30, 30, " + solidity + ")";
+  ctx.fillStyle = "rgba(250, 20, 20," + solidity + ")";
   ctx.fillRect(bX, bY, 100, 75);
-  }
+  ctx.fillRect(150, 500, bossHealth * 2, 40);
+  ctx.fillStyle = "#fff";
+  ctx.fillText("Sceris, Spirit of Evil health: " + bossHealth + "/250", 200, 550);
  }
-function barricaded() {
- if (fireball == false) {
-  bY = 75;
-  function randomX(min, max) {
-   bX = Math.random() * (max - min) + min;
-  }
-  randomX(75, 425);
-  fireBallDown();
- }
-}
-function fireBallDown() {
- fX = bX + 15;
-   fY = bY + 45;
+ c.onclick = function fire() {
+  if (fireball == false) {
+   dX = event.pageX;
+   dY = event.pageY;
+   fX = pX + 10;
+   fY = pY + 10; 
    fireball = true;
-   fDirect = "down";
+  } else if (fireball == true) {
+   fX += dX/100;
+   fY += dY/100;
+   if (fY > dY - 10 && fY < dY + 10 && fX > dX - 10 && fX < dX + 10) {
+    fireball = false;
+   }
+   ctx.fillStyle = "#c97f1d";
+   ctx.fillRect(fX, fY, 30, 30);
+  }
+ }
 }
