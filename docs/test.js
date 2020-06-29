209234b2ffa2e05da2e0f58ad5096d926ebca332
Ethan <48872919+egrayum@users.noreply.github.com>
2020-06-29 12:57:51 -0600

    keys to start instead of click

--- docs/test.js
+++ docs/test.js
@@ -3,5 +3,5 @@ function menu() {
   ctx.fillRect(0, 0, c.width, c.height);
   ctx.font = "60px Arial";
   ctx.fillStyle = "#000";
-  ctx.fillText("Thing", 300, 100);
+  ctx.fillText("Snaze Maze 2", 200, 100);
 }
